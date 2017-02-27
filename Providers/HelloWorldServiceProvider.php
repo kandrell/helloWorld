2d4e16405ab7f1e013f69a22a8788ada7e5b73dd
kandrell <andreas.leonhardt@plentymarkets.com>
2017-02-27 23:33:58 +0100

    Update HelloWorldServiceProvider.php

--- Providers/HelloWorldServiceProvider.php
+++ Providers/HelloWorldServiceProvider.php
@@ -17,7 +17,6 @@ class HelloWorldServiceProvider extends ServiceProvider
 
     }
 }
-...
        public function register()
        {
            $this->getApplication()->register(HelloWorldRouteServiceProvider::class);
